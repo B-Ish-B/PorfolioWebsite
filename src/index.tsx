25969838fd87e6aadcf022b57011cd6c5b785718
B-Ish-B <IshDesign@protonmail.com>
2025-04-16 00:41:58 -0500

    Clean slate for Testing branch

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.css';
-import { render } from 'react-dom';
-import { App } from './App';
-import * as THREE from 'three';
-// Make THREE available globally for debugging
-window.THREE = THREE;
-render(<App />, document.getElementById('root'));
--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Scene3D } from '../components/Scene3D';
-export const Home = () => {
-  return <div className="w-full h-screen">
-      <Scene3D />
-    </div>;
-};
